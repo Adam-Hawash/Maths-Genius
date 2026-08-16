d122d803b6559d0b6189c4c835f90ec0a6f10876
Adam Hawash <adamhawash03@gmail.com>
2026-08-16 16:15:18 +0300

    Add galleryImages state and setter to app store

--- src/stores/app-store.ts
+++ src/stores/app-store.ts
@@ -175,7 +175,8 @@ interface AppState {
 
   stats: Stats | null
   setStats: (stats: Stats | null) => void
-
+  galleryImages: GalleryImage[]
+  setGalleryImages: (images: GalleryImage[]) => void
   logout: () => void
 }
 
@@ -217,7 +218,8 @@ export const useAppStore = create<AppState>((set) => ({
 
   stats: null,
   setStats: (stats) => set({ stats }),
-
+  galleryImages: GalleryImage[]
+  setGalleryImages: (images: GalleryImage[]) => void
   logout: () =>
     set({
       currentStudent: null,
